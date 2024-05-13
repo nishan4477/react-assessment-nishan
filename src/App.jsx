ebad3567fd634e6a0063be34adb32ac945f2cf88
nishan4477 <nishansakha@gmail.com>
2024-05-13 21:21:36 +0545

    completed the task 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import React, { useState } from "react";
 import "./App.css";
 import { ShoppingListItem } from "./components/ShoppingListItem";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [shoppingList, setShoppingList] = useState([]);
+
+  function handleAddItem(value) {
+    if (!Boolean(value)) {
+      alert("Please enter the item before adding");
+    } else {
+      const isPresent = shoppingList?.findIndex((i) => i === value);
+      if (isPresent !== -1) {
+        alert("Item already present in the list");
+      } else {
+        setShoppingList((prev) => [...prev, value]);
+      }
+      setNewItem("");
+    }
+  }
+
+  function handleRemoveItem(item) {
+    setShoppingList(shoppingList?.filter((i) => i !== item));
+  }
+
   return (
     <div className="container">
       <h1 className="mb-4">My Shopping List</h1>
@@ -11,13 +33,22 @@ function App() {
           type="text"
           placeholder="E.g. Carrots"
           className="v__input flex-1"
+          onChange={(e) => setNewItem(e.target.value)}
+          value={newItem}
         />
-        <button className="v__button">Add</button>
+        <button className="v__button" onClick={() => handleAddItem(newItem)}>
+          Add
+        </button>
       </div>
       <div className="v__list-container overflow-y-scroll">
-        {/* Map your data here: */}
-        <ShoppingListItem />
-        <ShoppingListItem />
+        {shoppingList?.length > 0 &&
+          shoppingList?.map((item, index) => (
+            <ShoppingListItem
+              key={index}
+              item={item}
+              handleRemoveItem={handleRemoveItem}
+            />
+          ))}
       </div>
     </div>
   );
--- src/components/ShoppingListItem.jsx
+++ src/components/ShoppingListItem.jsx
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import classes from "./ShoppingListItem.module.css";
 
-export const ShoppingListItem = () => {
+export const ShoppingListItem = ({ item, handleRemoveItem }) => {
+  const [isChecked, setIsChecked] = useState(false);
   return (
     <div className="flex items-center p-2">
-      <input type="checkbox" className="mr-2" />
-      <h3 className="flex-1">Carrots</h3>
-      <button className={classes.removeButton}>x</button>
+      <input
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+        type="checkbox"
+        className="mr-2"
+      />
+      <h3 className={`flex-1 ${isChecked ? "line-through" : ""}`}>{item}</h3>
+      <button
+        onClick={() => handleRemoveItem(item)}
+        className={classes.removeButton}
+      >
+        x
+      </button>
     </div>
   );
 };
