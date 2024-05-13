ce4bfb79a4b825fc1e04b9d616d90ef1202db695
nishan4477 <nishansakha@gmail.com>
2024-05-13 16:52:04 +0545

    completed the task 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import Child from "./components/Child";
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <h1>0</h1>
-      <Child />
-      <p>Why so negative?</p>
+      <h1>{counter}</h1>
+      <Child counter={counter} setCounter={setCounter} />
+      {counter < 0 ? <p>Why so negative?</p> : ""}
     </div>
   );
 };
--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,10 +1,13 @@
-const Child = () => {
+const Child = ({counter,setCounter}) => {
   const handleMinusClick = () => {
     /* Implement logic here */
+    setCounter((prev)=>prev-1)
+
   };
 
   const handlePlusClick = () => {
     /* Implement logic here */
+    setCounter((prev)=>prev+1);
   };
 
   return (
